418e6f50b9dee6c540880a512e202e789b64dadb
Pedro Henrique <pedroh.01202@gmail.com>
2024-04-23 15:40:37 -0300

    add class 2, add class 3

--- 5-SQL/class-01-type-commands.sql
+++ 5-SQL/class-01-type-commands.sql
@@ -1,12 +1,45 @@
--- DDL
-DROP TABLE students;
-CREATE TABLE IF NOT EXISTS students (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    cpf TEXT NOT NULL UNIQUE,
-    age NUMERIC
-);    
+CREATE DATABASE school;
+DROP DATABASE school;
 
---DML
-INSERT INTO students (name,cpf, age)
-VALUES ('Pedro Henrique', '606.372.713-27', 31);
+USE school;
+
+CREATE TABLE students(
+	named VARCHAR(50) NOT NULL,
+    age INT NOT NULL
+);
+
+DROP TABLE students;
+
+SELECT * FROM students;
+DESCRIBE students;
+
+INSERT INTO students(named, age) VALUES("Samila", 18);
+INSERT INTO students VALUES('Emanuel', 30, DEFAULT);
+INSERT INTO students(age, named) VALUES(16, 'Murilo');
+
+SET SQL_SAFE_UPDATES = 1;
+
+UPDATE students SET age = 30 WHERE named = 'Emanuel';
+DELETE FROM students WHERE named = 'Emanuel';
+-- DELETE FROM students;
+
+TRUNCATE students;
+
+ALTER TABLE students 
+ADD COLUMN city VARCHAR(20) NOT NULL DEFAULT 'Jucás'
+AFTER age;
+
+ALTER TABLE students 
+ADD COLUMN id INT
+FIRST;
+
+ALTER TABLE students DROP COLUMN id;
+
+ALTER TABLE students 
+MODIFY COLUMN named VARCHAR(30) NOT NULL;
+
+ALTER TABLE estudantes RENAME TO students;
+ALTER TABLE students RENAME COLUMN named TO nome;
+
+ALTER TABLE students 
+CHANGE COLUMN nome named VARCHAR(30) NOT NULL;
